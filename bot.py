79063e6dbce53c4cc234a38a2971a4d49e02fa90
Tydus <Tydus@Tydus.org>
2018-06-19 19:25:17 +0800

    Fuck Python3

--- bot.py
+++ bot.py
@@ -289,7 +289,7 @@ class BloodBoundGame:
         if len(self.players) % 2 == 1:
             self.available_curse = ["curse%d" % i for i in range(4)]
 
-            size = min(2, (len(self.players) - 3) / 2)
+            size = max(2, (len(self.players) - 3) // 2)
             self.available_curse = self.available_curse[:size]
             self.real_curse = random.randint(0, size - 1)
         else:
